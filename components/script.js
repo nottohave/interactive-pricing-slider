5a3e37ccd9abe59e1d46e942e04a1250af92170d
nottohave <nottohave@gmail.com>
2022-01-27 19:30:22 -0800

    update slider value func and discount function

--- components/script.js
+++ components/script.js
@@ -1,35 +1,54 @@
-
 var slider = document.getElementById("myRange");
 var dollarOutput = document.getElementById("dollar");
 var viewsOutput = document.getElementById("output");
+// toggle values
+var checkbox = document.querySelector(".inp_checkbox");
 
-output.innerHTML = slider.value; // Display the default slider value
-dollarOutput.innerHTML = "$" + (((slider.value)/ 10) * 4) + ".00";
-viewsOutput.innerHTML = "100K";
+// first load, default value is 100k - $16
+output.textContent = slider.value;
+dollarOutput.textContent = slider.value * 4;
+viewsOutput.textContent = "100K";
+checkbox.checked = false;
+
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+
+  // turn off the switch everytime we slide
+  checkbox.checked = false;
+
+  let newSliderValue = slider.value;
+  dollarOutput.textContent = newSliderValue * 4;
+
   // list the condition 
-  dollarOutput.innerHTML = "$" + (((slider.value)/ 10) * 4) + ".00";
-
-  if (dollarOutput.innerHTML === "$8.00") {
-    viewsOutput.innerHTML = "10K";
-  } else if(dollarOutput.innerHTML === "$12.00") {
-    viewsOutput.innerHTML = "50K";
-  } else if(dollarOutput.innerHTML === "$16.00") {
-    viewsOutput.innerHTML = "100K";
-  } else if(dollarOutput.innerHTML === "$20.00") {
-    viewsOutput.innerHTML = "250K";
-  } else if(dollarOutput.innerHTML === "$24.00") {
-    viewsOutput.innerHTML = "500K";
-  } else if(dollarOutput.innerHTML === "$28.00") {
-    viewsOutput.innerHTML = "600K";
-  } else if(dollarOutput.innerHTML === "$32.00") {
-    viewsOutput.innerHTML = "700K";
-  } else if(dollarOutput.innerHTML === "$36.00") {
-    viewsOutput.innerHTML = "1M";
+  if (newSliderValue == 2) {
+     viewsOutput.textContent = "10K";
+  } else if(newSliderValue == 3) {
+    viewsOutput.textContent = "50K";
+  } else if(newSliderValue == 4) {
+    viewsOutput.textContent = "100K";
+  } else if(newSliderValue == 5) {
+    viewsOutput.textContent = "250K";
+  } else if(newSliderValue == 6) {
+    viewsOutput.textContent = "500K";
+  } else if(newSliderValue == 7) {
+    viewsOutput.textContent = "600K";
+  } else if(newSliderValue == 8) {
+    viewsOutput.textContent = "700K";
+  } else if(newSliderValue == 9) {
+    viewsOutput.textContent = "1M";
   }
+}
 
-} 
+// switch function here
+// switch on: take the monthly price value and times 25% discount
+// switch off: return to original price
+function applyDiscount() {
+  if(checkbox.checked !== false) {
+    dollarOutput.textContent = parseInt(dollarOutput.textContent) * 0.25;
+  } else {
+    dollarOutput.textContent = slider.value * 4;
+  }
+}
+
 
